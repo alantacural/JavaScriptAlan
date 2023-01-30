482a2dc10eb4f9caad1f36cd0d435e7c7d07e73d
Alan Rodriguez <110635964+alantacural@users.noreply.github.com>
2023-01-30 16:52:15 -0300

    Add files via upload

---
+++ Leccion2.js
@@ -0,0 +1,10 @@
+var nombre="Alan";
+var edad=32;
+var desarrollador= true;
+const fecha=new Date(1990,4,21);
+const libro={
+    nombre:"Fundamentos de programacion en Java",
+    autor: "Jorge Martinez",
+    fecha: new Date(2020,5,9),
+    url: "https://www.tesuva.edu.co/phocadownloadpap/Fundamentos%20de%20programcion%20en%20Java.pdf"
+}
