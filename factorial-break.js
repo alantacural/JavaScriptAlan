e9e1742242a30c4a8808cb449cc0dd5a42209747
Alan Rodriguez <110635964+alantacural@users.noreply.github.com>
2023-02-02 10:46:14 -0300

    Add files via upload

---
+++ factorial-break.js
@@ -0,0 +1,12 @@
+i=10
+let factorial=1
+while(true)
+{
+    if(i=0)
+    {
+        break
+    }
+    factorial = factorial*i
+    i--
+}
+console.log(factorial)
---
+++ factorial-while.js
@@ -0,0 +1,8 @@
+i=10
+let factorial=1
+while(i>0)
+{
+    factorial = factorial*i
+    i--
+}
+console.log(factorial)
