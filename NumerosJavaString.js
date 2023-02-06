b0c1164dbd14dc7ba6f02f65911ccde117c3445e
Alan Rodriguez <110635964+alantacural@users.noreply.github.com>
2023-02-06 12:28:56 -0300

    Add files via upload

---
+++ NumerosJavaString.js
@@ -0,0 +1,7 @@
+let alturacent = 182
+let alturametros = 1.82
+let peso = 88.5
+let altredo= alturametros.toFixed(0)
+let pesoredon= Math.floor(peso)
+console.log(pesoredon)
+let max= "el máximo valor que se puede obtener en Javascript + 1 es "+ Number.MAX_VALUE
